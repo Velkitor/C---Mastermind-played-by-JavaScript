a66655f9dd3f3b96bf624be78eb7a1b5928f1c6b
Andrew Hecht <andrewh@lightspeedsystems.com>
2012-10-15 19:39:41 -0700

    Clean out some of the Intermediate data that doesn't need to be sent to GitHub

--- DerivedData/Build/Intermediates/ArchiveIntermediates/JSGame/IntermediateBuildFilesPath/JSGame.build/Release/JSGame.build/Objects-normal/x86_64/Player.d
+++
@@ -1,8 +0,0 @@
-dependencies: /Users/andy/src/JSGame/JSGame/Player.cpp \
-  /Users/andy/src/JSGame/JSGame/main.h \
-  /Users/andy/src/google_v8/include/v8.h \
-  /Users/andy/src/google_v8/include/v8stdint.h \
-  /Users/andy/src/JSGame/JSGame/Player.h \
-  /Users/andy/src/JSGame/JSGame/gfxBox.h \
-  /Users/andy/src/JSGame/JSGame/Display.h \
-  /Users/andy/src/JSGame/JSGame/javascript_main.h
--- DerivedData/Build/Intermediates/JSGame.build/Debug/JSGame.build/Objects-normal/x86_64/glfont2.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/andy/src/JSGame/JSGame/glfont2.cpp \
-  /Users/andy/src/JSGame/JSGame/glfont2.h
